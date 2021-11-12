e85a9801eae55909d5b81514f4f9254a4c012fb9
AyumuTakai <atakai@hackwork.jp>
2021-11-13 00:26:02 +0900

    Create vivliostyle.config.js

---
+++ vivliostyle.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Theme取得ライブラリの開発',
+  author: 'Ayumu Takai',
+  language: 'ja',
+  size: 'A4 landscape',
+  theme: ['@vivliostyle/theme-slide'],
+  entry: [
+    'slides.md',
+  ],
+  output: 'slide.pdf',
+  workspaceDir: ".vivliostyle",
+};
